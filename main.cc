3c3abda026a13d34b89e3ab8c37fd4f25732472f
Sachin Krishnan <sachkris@gmail.com>
2016-09-26 17:45:27 +0530

    get_doi works for arXiv articles. get_doi returns url

--- main.cc
+++ main.cc
@@ -9,38 +9,57 @@ using namespace std;
 
 string get_doi(char *filename)
 {
+	int i;
 	string page_content;
 	string doi;
 	size_t first;
 	size_t last;
 
 	poppler::document *pdfdoc = poppler::document::load_from_file(filename);
-	for (int i = 0; i < pdfdoc->pages; ++i)
+	for (i = 0; i < 2; ++i)
 	{
 		page_content = pdfdoc->create_page(i)->text().to_latin1();
+		cout<<"$$$$$$$$$$$$"<<endl<<page_content<<endl;
 		first = page_content.find("dx.doi.org");
 		if(first != string::npos)
 		{
-			end = page_content.find(" ", first);
-			doi = page_content.substr(first+11, end-start-11);
+			last = page_content.find(" ", first);
+			doi = page_content.substr(first+11, last-first-11);
+			doi = doi.insert(0, "http://dx.doi.org/");
 			break;
 		}
 		
 		first = page_content.find("DOI");
 		if(first != string::npos)
 		{
-			end = page_content.find(" ", first);
-			doi = page_content.substr(first+5, end-start-5);
+			last = page_content.find(" ", first+5);
+			doi = page_content.substr(first+5, last-first-5);
+			doi = doi.insert(0, "http://dx.doi.org/");
 			break;
 		}
 
 		first = page_content.find("doi");
 		if(first != string::npos)
 		{
-			end = page_content.find(" ", first);
-			doi = page_content.substr(first+4, end-start-4);
+			last = page_content.find(" ", first);
+			doi = page_content.substr(first+4, last-first-4);
+			doi = doi.insert(0, "http://dx.doi.org/");
 			break;
 		}
+
+		first = page_content.find("arXiv");
+		if(first != string::npos)
+		{
+			last = page_content.find(" ", first);
+			doi = page_content.substr(first+6, last-first-6);
+			doi = doi.insert(0, "http://arxiv.org/abs/");
+			break;
+		}
+	}
+	for(i = 0;i < doi.length();i++)
+	{
+		if(doi[i] == '\n')
+			doi.erase(i,1);
 	}
 	return doi;
 }
